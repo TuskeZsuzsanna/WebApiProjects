88d432a41b5819fd897e231bf2dae7a9713ffea6
TuskeZsuzsanna <tuskezsuzsi2004@gmail.com>
2023-11-28 09:06:04 +0100

    objektumok, tömbök, fetch

---
+++ WebAPIBase/wwwroot/geek.js
@@ -0,0 +1,28 @@
+﻿var viccek;
+
+windows.onload(){
+    Letöltés();
+}
+
+fetch('/jokes.json'){
+    .then(response => response.json())
+    .then(data => LetöltésBefejeződött(data))
+}
+
+
+function Letöltés() {
+    
+}
+
+
+function LetöltésBefejeződött(d) {
+    console.log("Sikeres letöltés.")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        let elem = document.createElement("li")
+        elem.innerHTML = viccek[i].questionText
+        ide.appendChild(elem)
+    }
+}
